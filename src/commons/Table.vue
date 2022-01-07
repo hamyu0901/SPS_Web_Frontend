ec9831bbe3760a7047845a1deb4a65a2326bd48b
봉시윤 <siyoon@doolim-yaskawa.com>
2022-01-07 17:30:05 +0900

    1. 바 차트 데이터 수정 및 UI 추가 2. 날짜 계산 기능 추가

--- src/commons/Table.vue
+++ src/commons/Table.vue
@@ -1,18 +1,21 @@
 <template>
-  <table style="height: 250px;">
+  <table class="no-drag" style="height: 250px;">
     <thead>
         <tr style="height: 30px;">
             <th></th>
-            <th v-for="(x_header, index) in propsTableData.x_Headers" :key="index">
+            <th v-for="(x_header, index) in propsTableData.x_Headers" :key="index" :class="x_value === index ? 'active' : null">
                 <label>{{x_header.name}}</label>
             </th>
         </tr>
     </thead>
     <tbody>
-        <tr v-for="(y_header, index) in propsTableData.y_Headers" :key="index">
-            <th>
+        <tr v-for="(y_header, index) in propsTableData.y_Headers" :key="index" >
+            <th :class="y_value === index ? 'active' : null">
                 <label>{{y_header.name}}</label>
             </th>
+            <td v-for="(data, data_index) in y_header.dataList" :key="data_index" @mouseover="mouseover(index, data_index)" @mouseleave="mouseleave()">
+                <label>{{data}}</label>
+            </td>
         </tr>
     </tbody>
 </table>
@@ -23,7 +26,20 @@ export default {
     props: ["propsTableData"],
     data(){
         return{
+            isActive : false,
+            y_value: null,
+            x_value: null,
+        }
+    },
+    methods:{
+        mouseover(y, x){
+            this.y_value = y;
+            this.x_value = x;
 
+        },
+        mouseleave(){
+            this.y_value = null;
+            this.x_value = null;
         }
     }
 }
@@ -37,7 +53,7 @@ table{
     overflow: auto;
     -ms-overflow-style: none; /* IE and Edge */
     scrollbar-width: none; /* Firefox */
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 20px rgba(116, 115, 115, 0.651);
     border: 1px solid white;
 
     thead{
@@ -48,13 +64,55 @@ table{
         th{
             border-left: 1px solid white;
             border-bottom: 1px solid white;
+
+            &.active{
+                background-color: white;
+                color: black;
+            }
         }
     }
     tbody{
-        th{
-            border-top: 1px solid white;
-            border-right: 1px solid white;
+        tr{
+            border-bottom: 1px solid white;
+
+            th{
+                border-top: 1px solid white;
+                border-right: 1px solid white;
+                background-color: none;
+
+                 &.active{
+                    background-color: white;
+                    color: black;
+                }
+            }
+            
+            
+            td{
+                border-right: 1px solid white;
+
+                label{
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+
+                &:hover{
+                    background-color:yellow;
+                    color: black;
+                }
+
+                &:last-child{
+                    border-right: none;
+                }
+            }
+
+            &:last-child{
+                border-bottom: none;
+            }
         }
+        
     }
 }
 </style>
--- src/components/diagnostics/report/report/tempAnalysis/TempAnalysis.vue
+++ src/components/diagnostics/report/report/tempAnalysis/TempAnalysis.vue
@@ -11,46 +11,55 @@
         bottom offset-y
     >
         <template v-slot:activator="{ on }">
-            <label @click="decreaseMonth()" style="margin-right: 30px">{{'<'}}</label>
-            <label v-on="on">{{selectedMonth.format('YYYY년 MM월')}}</label>
-            <label @click="increaseMonth()" style="margin-left: 30px">{{'>'}}</label>
+            <v-btn @click="decreaseMonth()" color="grey darken-1" :ripple="false">
+                <v-icon class="no-drag">arrow_back_ios</v-icon>
+            </v-btn>
+            <label class="no-drag" v-on="on">{{curr_month.format('YYYY년 MM월')}}</label>
+            <v-btn @click="increaseMonth()" color="grey darken-1" :ripple="false">
+                <v-icon class="no-drag">arrow_forward_ios</v-icon>
+            </v-btn>
         </template>
-        <v-date-picker v-model="month" color="green lighten-1" type="month" locale="ko-kr" id="dateHeader">
+        <v-date-picker v-model="time" color="green lighten-1" type="month" locale="ko-kr" id="dateHeader">
              <v-spacer></v-spacer>
           <v-btn flat color="primary" @click="menu = false">취소</v-btn>
           <v-btn flat color="primary" @click="changeMonth()">확인</v-btn>
         </v-date-picker>
     </v-menu>
-    </div>
+    </div>ㄴ
     <div id="tableContainer">
         <div id="tablePrensent">
-            <div class="periodBox">{{curr_month.format('MM')}}월 축별 온도 평균 데이터</div>
+            <div class="periodBox">{{curr_month.format('YYYY년 MM')}}월 축별 온도 평균 데이터</div>
             <div class="tableBox">
                 <table-vue :propsTableData="propsTableData"></table-vue>
             </div>
         </div>
         <div id="tableBefore">
-            <div class="periodBox">{{curr_month.subtract(1, 'M').format('MM')}}월 축별 온도 평균 데이터</div>
+            <div class="periodBox">{{prev_month}}월 축별 온도 평균 데이터</div>
             <div class="tableBox">
                 <table-vue :propsTableData="propsTableData"></table-vue>
             </div>
         </div>
     </div>
-    
+    <div id="chartContainer">
+        <bar-chart></bar-chart>
+    </div>
   </div>
 </template>
-
+ㄴ
 <script>
 import moment from 'moment';
 import TableVue from '../../../../../commons/Table.vue'
+import BarChartVue from '../../../../../commons/BarChart.vue';
 export default {
     components: {
-        TableVue: TableVue
+        TableVue: TableVue,
+        barChart: BarChartVue
     },
     data: () => ({
-        month: new Date().toISOString().substr(0, 7),
-        selectedMonth: moment(),
-        curr_month: moment(),
+        time: null,
+        selectedMonth: null,
+        curr_month: null,
+        prev_month: null,
         menu: false,
         propsTableData:{
             x_Headers:[
@@ -62,32 +71,61 @@ export default {
                 {name: '6축'},
             ],
             y_Headers:[
-                {name: 'L1'},
-                {name: 'R1'},
-                {name: 'L2'},
-                {name: 'R2'},
-                {name: 'L3'},
-                {name: 'R3'},
-                {name: 'L4'},
-                {name: 'R4'},
+                {
+                    name: 'L1',
+                    dataList: [8, 3, 3, 40, 2, 11]    
+                },
+                {
+                    name: 'R1',
+                    dataList: [2, 10, 3, 13, 2, 11]    
+                },
+                {
+                    name: 'L2',
+                    dataList: [33, 11, 7, 43, 2, 11]    
+                },
+                {
+                    name: 'R2',
+                    dataList: [1, 15, 3, 43, 2, 11]    
+                },
+                {
+                    name: 'L3',
+                    dataList: [12, 1, 3, 43, 2, 11]    
+                },
+                {
+                    name: 'R3',
+                    dataList: [21, 0, 3, 43, 2, 11]    
+                },
+                {
+                    name: 'L4',
+                    dataList: [18, 10, 3, 43, 2, 11]    
+                },
+                {
+                    name: 'R4',
+                    dataList: [9, 10, 3, 43, 2, 11]    
+                },
             ]
         }
     }),
+    created(){
+        this.time = new Date().toISOString();
+        this.curr_month = moment(this.time);
+        this.prev_month = moment(this.time).subtract(1, 'M').format('YYYY년 MM');
+    },
     methods:{
         changeMonth(){
-            this.selectedMonth = moment(this.month);
-            this.curr_month = moment(this.month);
+            this.curr_month = moment(this.time);
+            this.prev_month = moment(this.time).subtract(1, 'M').format('YYYY년 MM');
             this.menu = false;
         },
         increaseMonth(){
-            this.month = moment(this.month).add(1, 'M');
-            this.selectedMonth = this.month;
-            this.curr_month = moment(this.month);
+            this.time.setMonth(this.time.getMonth() + 1);
+            this.curr_month = moment(this.time);
+            this.prev_month = moment(this.time).subtract(1, 'M').format('YYYY년 MM');
         },
         decreaseMonth(){
-            this.month = moment(this.month).subtract(1, 'M');
-            this.selectedMonth = this.month;
-            this.curr_month = moment(this.month);
+            this.time.setMonth(this.time.getMonth() - 1);
+            this.curr_month = moment(this.time);
+            this.prev_month = moment(this.time).subtract(1, 'M').format('YYYY년 MM');
         }
 
     }
