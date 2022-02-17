dbc94660f8a48b5992bd5bed701f72e1fe256e44
yujin <yujin@doolim-yaskawa.com>
2022-02-17 11:33:01 +0900

    devextreme css 적용 고정 컴포넌트

--- src/components/diagnostics/report/report/torqueAnalysis/TorqueReport.vue
+++ src/components/diagnostics/report/report/torqueAnalysis/TorqueReport.vue
@@ -5,7 +5,6 @@
 </template>
 <script>
 </script>
-
 <style>
     .v-input{
         padding-top: 0px;
@@ -21,12 +20,32 @@
         background-color: #515dbe!important;
         text-align: center!important;
     }
+    .dx-data-header .robot-highlighted {
+        background-color: #3c78fa;
+    }
     .dx-data-row .prevRobot-highlighted {
         background-color: #21976a!important;
         text-align: center!important;
     }
     .prevSelectContent .dx-selectbox-container .dx-texteditor-container  {
         background: #191d2b;
+        border-radius: 5px;
+    }
+    .dx-header-row .robot-highlighted {
+        text-align: center!important;
+        color: white;
+    }
+    .dx-header-row .axis-highlighted {
+        text-align: center!important;
+        color: white;
+    }
+    .dx-header-row .danger-highlighted {
+        text-align: center!important;
+        color: white;
+    }
+    .dx-header-row .comment-highlighted {
+        text-align: center!important;
+        color: white;
     }
 /* .dx-datagrid-focus-overlay {
     border: 2px solid rgb(218, 78, 78);
